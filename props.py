7d4fc584c5fdd0c59cde18dc2092dc7974dec5d6
KowsarAtz <kowsar.atazadeh@gmail.com>
2019-08-17 11:05:17 +0430

    mongo cluster 1 results

--- props.py
+++ props.py
@@ -13,13 +13,13 @@ HOSTIP = "172.16.13.26"
 BASEPATH = "/mnt/hdd2/nosql-prj/YCSB/YCSB/"
 RESULTPATH = "./ycsb-results/"
 MONGO_URL = "mongodb://%s:27023" % HOSTIP
-MONGO_CLUSTER_URL = None
+MONGO_CLUSTER_URL = "mongodb://%s:27021" % HOSTIP
 SQL_EXPORT_DB = 'ycsb'
 RECORDS_COL = 'usertable'
 MSSQLUSER = 'sa'
 MSSQLPWD = 'MSSql-pwd'
 
-DBS = [MSSQL, MONGODB]
+DBS = [MONGODB_CLUSTER]
 WORKLOADS = ['a', 'b', 'c', 'd', 'e', 'f']
 RC_OP_COUPLES = [(1000, 1000)]
 # THREADS_NO = [1] + list(range(2, 33, 2))

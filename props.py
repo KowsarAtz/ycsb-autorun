ecee3b0384fe1f8960b23f4db3a32a0448c642df
KowsarAtz <kowsar.atazadeh@gmail.com>
2019-08-17 10:52:04 +0430

    new result set

--- props.py
+++ props.py
@@ -9,20 +9,20 @@ REQS2 = None
 mssql_connection = None
 mongodb_client = None
 
-HOSTIP = "127.0.0.1"
-BASEPATH = "/home/atz2nd/Desktop/editycsb/YCSB/"
+HOSTIP = "172.16.13.26"
+BASEPATH = "/mnt/hdd2/nosql-prj/YCSB/YCSB/"
 RESULTPATH = "./ycsb-results/"
-MONGO_URL = "mongodb://%s:27017" % HOSTIP
+MONGO_URL = "mongodb://%s:27023" % HOSTIP
 MONGO_CLUSTER_URL = None
 SQL_EXPORT_DB = 'ycsb'
 RECORDS_COL = 'usertable'
 MSSQLUSER = 'sa'
-MSSQLPWD = 'MSSQL-pwd'
+MSSQLPWD = 'MSSql-pwd'
 
 DBS = [MSSQL, MONGODB]
 WORKLOADS = ['a', 'b', 'c', 'd', 'e', 'f']
-RC_OP_COUPLES = [(10, 5), (20, 10)]
+RC_OP_COUPLES = [(1000, 1000)]
 # THREADS_NO = [1] + list(range(2, 33, 2))
-THREADS_NO = [1, 2, 4]
+THREADS_NO = [1, 2, 4, 8, 12, 16]
 RUNS_NO = 3
-LOAD_TC = 4
+LOAD_TC = 8
